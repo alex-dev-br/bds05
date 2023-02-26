9eb30cb68dac55b135309c8ad63de71d47af5ea1
Alexandre Andrade <alex_dev@outlook.com.br>
2023-02-26 04:38:52 -0300

    feat(entities): Genre, Movie, Review, Role, User

--- src/test/java/com/devsuperior/movieflix/entities/GenreTests.java
+++ src/test/java/com/devsuperior/movieflix/entities/GenreTests.java
@@ -1,5 +1,6 @@
 package com.devsuperior.movieflix.entities;
 
+import entities.Genre;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
--- src/test/java/com/devsuperior/movieflix/entities/MovieTests.java
+++ src/test/java/com/devsuperior/movieflix/entities/MovieTests.java
@@ -1,5 +1,7 @@
 package com.devsuperior.movieflix.entities;
 
+import entities.Genre;
+import entities.Movie;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
